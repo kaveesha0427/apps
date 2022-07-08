f725c51090396fc4f2028c5660d18e723c688993
kaveesha0427 <https://github.com/kaveesha0427>
2022-07-08 15:46:40 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:apps/transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  Main({Key? key}) : super(key: key);
+
+  final List<Transactions> transactions = [
+    Transactions(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transactions(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 15.78,
+        date: DateTime.now())
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('HI'),
+        ),
+        body: Column(
+          children: <Widget>[
+            const SizedBox(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Chart'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactions.map(
+                (tx) {
+                  return Card(
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Text(
+                            tx.amount.toString(),
+                          ),
+                        ),
+                        Row(
+                          children: [Text(tx.title), 
+                          Text(tx.date.toString())],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
