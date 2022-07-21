e5a64104cec3f67112d7a2327e17d0402ba9f568
kaveesha0427 <https://github.com/kaveesha0427>
2022-07-21 10:30:26 +0530

    added TextEditingControllers

---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,19 @@
+import 'package:apps/widgets/new_transaction.dart';
+import 'package:apps/widgets/transaction_list.dart';
+import 'package:flutter/material.dart';
+
+class UserTransactions extends StatefulWidget {
+  const UserTransactions({Key? key}) : super(key: key);
+
+  @override
+  State<UserTransactions> createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [NewTransaction(),TransactionList()],
+    );
+  }
+}
